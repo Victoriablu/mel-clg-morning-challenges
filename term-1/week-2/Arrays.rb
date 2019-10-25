7ed48c5d5a17205abc48ceab89a98fc0ed6a38fc
Victoriablu <Victoriablu@users.noreply.github.com>
2019-10-25 15:05:58 +1100

    Adding Arrays challenge

---
+++ term-1/week-2/Arrays.rb
@@ -0,0 +1,22 @@
+shopping_list = ["Milk","Bread","Eggs","Steak","Pork","Chicken","Fish","Salad Mix","Avacado","Asparagus","Mushrooms","Grapefruit","Dog Food","Cat Food"]
+puts shopping_list.length #prints length of array
+puts shopping_list[4] #prints the 5th item
+puts shopping_list.first #prints first item
+puts shopping_list.last #prints the last item
+puts shopping_list[-1] #prints the last item
+puts shopping_list #prints the whole list
+puts shopping_list[shopping_list.length/2] #prints the middle item
+puts shopping_list[rand(5)] #generates a random item between 0 & 5
+shopping_list.push("Cheese") # adds an item to the end of the array
+puts shopping_list.last 
+shopping_list.unshift("Oranges") #adds an item to the start of the array
+puts shopping_list.first
+puts shopping_list.shift #removes the first item in array and prints it
+puts shopping_list.pop #removes the last item in array and prints it
+puts shopping_list.length
+
+
+
+
+
+
