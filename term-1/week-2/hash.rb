1e4717c42451ee6d8077f8a9b2bdc06de6908a66
Victoriablu <Victoriablu@users.noreply.github.com>
2019-11-01 09:11:42 +1100

    Challenge updates

--- term-1/week-2/hash.rb
+++ term-1/week-2/hash.rb
@@ -2,3 +2,8 @@ position = {:alan => "educator", :rach => "lead educator", :tayla => "operations
 #Hash - Key on left side then the value associated with the key
 # Use symbols :example as keys instead of strings
 
+web_params = {name: "steph", age: "34", interests: ["Video Games", "Board Games", "Travelling"]}
+puts web_params[:interests][2]
+puts web_params[:name].class
+
+
