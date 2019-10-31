d3043a83b59c2cf673a6b34d4de408239c4f03ec
Victoriablu <Victoriablu@users.noreply.github.com>
2019-10-31 17:18:15 +1100

    Update for Simplebank

--- term-1/week-3/simplebank.rb
+++ term-1/week-3/simplebank.rb
@@ -4,7 +4,8 @@ account_balance = 0
 deposit = 0
 withdraw = 0
 
-puts "1. Deposit 2. Withdrawal 3. View Balance"
+def banking_app
+puts "1. Deposit 2. Withdrawal 3. View Balance 4.Exit"
 input = gets.chomp.to_i
 
 case input
@@ -15,5 +16,13 @@ when 1
 when 2
     puts "How much would you like to withdraw?"
     withdraw = gets.chomp.to_i
-    account_balance -= gets.chomp.to_i
-when 3 
+    account_balance -= withdraw
+when 3
+    puts "Your account balance is " + account_balance.to_s
+when 4
+    puts "Thank you for banking with us"
+    exit
+end 
+end
+
+banking_app
